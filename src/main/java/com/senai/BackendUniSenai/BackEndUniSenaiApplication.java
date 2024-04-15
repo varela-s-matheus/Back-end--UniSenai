893c4abbb0b5298a84f45db7792e648800ed11b5
Matheus Varela <matheus_s_varela@hotmail.com>
2024-04-15 19:45:54 -0300

    Initial commit

---
+++ src/main/java/com/senai/BackendUniSenai/BackEndUniSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.BackendUniSenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndUniSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndUniSenaiApplication.class, args);
+	}
+
+}
